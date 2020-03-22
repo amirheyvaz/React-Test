844490d1b322138ff036ed74f8132cc625d3ff97
amirheyvaz <amirheyvaz@gmail.com>
2020-03-22 21:17:30 +0430

    .

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import { render } from '@testing-library/react';
 import ValidationComponent from './ValidationComponent/ValidationComponent';
 import CharComponent from './CharComponent/CharComponent';
 
+
 class App extends Component {
   state = {
     inputLenght : null,
